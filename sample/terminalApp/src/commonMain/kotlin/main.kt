774957822a16227519348d4625aaa0f5653cbe8f
messias <messiaslima.03@gmail.com>
2025-06-16 14:26:11 +0100

    terminal app sample

--- sample/terminalApp/src/commonMain/kotlin/main.kt
+++ sample/terminalApp/src/commonMain/kotlin/main.kt
@@ -1,5 +1,12 @@
-import dev.appoutlet.getFibonacciNumbers
+import dev.appoutlet.umami.Umami
+import dev.appoutlet.umami.api.event
+import kotlinx.coroutines.runBlocking
 
-fun main() {
-    println("getFibonacciNumbers(7)=${getFibonacciNumbers(7).joinToString(", ")}")
+fun main() = runBlocking {
+    val umami = Umami.create(
+        baseUrl = "http://192.168.1.1:3000",
+        website = "45242d93-0acf-4103-9d8e-65325d615e81",
+    )
+
+    umami.event(title = "Hello World", url = "/hello-world", name = "terminal-app-event")
 }

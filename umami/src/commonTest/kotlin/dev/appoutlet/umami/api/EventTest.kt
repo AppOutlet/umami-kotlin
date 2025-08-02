4f7f4498a6ef6225aec70aff2c2987c275ceaf4c
Messias Junior <messiaslima.03@gmail.com>
2025-08-02 16:58:56 +0100

    create sample test

---
+++ umami/src/commonTest/kotlin/dev/appoutlet/umami/api/EventTest.kt
@@ -0,0 +1,11 @@
+package dev.appoutlet.umami.api
+
+import io.kotest.matchers.shouldBe
+import kotlin.test.Test
+
+class EventTest {
+    @Test
+    fun `sample test`() {
+        1 shouldBe 1
+    }
+}

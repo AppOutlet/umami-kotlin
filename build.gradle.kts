db92c6d6cd20fdd3d433dc91091a9a7db990e8bd
Messias Junior <messiaslima.03@gmail.com>
2026-04-12 17:58:01 +0100

    remove hot reload

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.compose.hotReload) apply false
     alias(libs.plugins.compose.multiplatform) apply false
     alias(libs.plugins.maven.publish) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
